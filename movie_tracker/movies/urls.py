1d37f25df0c6307a49aacf28d1eb035132f17570
Rachel Davey <racheld9999@outlook.com>
2025-09-24 01:24:47 +0200

    movie detail view

--- movie_tracker/movies/urls.py
+++ movie_tracker/movies/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from . import views
 
 urlpatterns = [
-
+    path('movies/<int:movie_id>', views.movie_detail, name="movie detail")
 ]
--- movie_tracker/movies/views.py
+++ movie_tracker/movies/views.py
@@ -1,3 +1,11 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
+
+def movie_detail(request, movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={settings.TMDB_API_KEY}&language=en-US " # api url
+    response = requests.get(url)
+    data = response.json()
+    return render(request, 'movies/movie_detail.html', {"movie": data})
+
