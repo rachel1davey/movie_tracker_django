6f60e28ede3948a58476ec60010522d33532fb95
Rachel Davey <racheld9999@outlook.com>
2025-09-24 14:45:43 +0200

    touching up model, review, movielist, profile

--- movie_tracker/core/models.py
+++ movie_tracker/core/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from allauth import User
+from django.contrib.auth import User
 
 # Create your models here.
-class MovieList(models.Model)
+class MovieList(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     
 
 class Profile(models.Model):
-    user = models.models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
     bio = models.CharField(_(""), max_length=50)
     avatar = models.ImageField
--- movie_tracker/movies/models.py
+++ movie_tracker/movies/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from allauth import User
+from django.contrib.auth import User
 
 # Create your models here.
 
 class Review(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.CharField(max_length="500")
-    rating = models.IntegerField(default=0)
-
+    body = models.CharField(max_length=500)
+    rating = models.IntegerField(default=0,validators=[MinValueValidator(1), MaxValueValidator(5)], movie_id = models.IntegerField(), created_at = models.DateTimeField(auto_now_add=True), updated_at = models.DateTimeField(auto_now=True)
+)
 
