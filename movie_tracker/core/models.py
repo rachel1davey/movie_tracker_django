3a112523ee53e620fb1d82452b2a76ed6b208c09
Rachel Davey <racheld9999@outlook.com>
2025-09-24 14:39:03 +0200

    basic model setup

--- movie_tracker/core/models.py
+++ movie_tracker/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from allauth import User
 
 # Create your models here.
+class MovieList(models.Model)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+
+class Profile(models.Model):
+    user = models.models.ForeignKey(User,on_delete=models.CASCADE)
+    bio = models.CharField(_(""), max_length=50)
+    avatar = models.ImageField
--- movie_tracker/movies/models.py
+++ movie_tracker/movies/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from allauth import User
 
 # Create your models here.
+
+class Review(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.CharField(max_length="500")
+    rating = models.IntegerField(default=0)
+
+
