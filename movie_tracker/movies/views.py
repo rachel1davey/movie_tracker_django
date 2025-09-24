849ccefc923c5416bb5105a2e038ef7f70c92277
Rachel Davey <racheld9999@outlook.com>
2025-09-25 00:35:41 +0200

    Add user review display and submission functionality in movie detail view

--- movie_tracker/movies/views.py
+++ movie_tracker/movies/views.py
@@ -1,6 +1,6 @@
 import requests
-from django.conf import settings 
-from django.shortcuts import render, redirect
+from django.conf import settings
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Review
 from .forms import ReviewForm
 
@@ -8,33 +8,27 @@ from .forms import ReviewForm
 # Create your views here.
 
 def movie_detail(request, movie_id):
-    reviews = Review.objects.filter(movie_id=movie_id)
-
-# review form post
-    if request.method == "POST":
-        if request.user.is_authenticated:
-            form = ReviewForm(request.POST)
-            if form.is_valid():
-                review = form.save(commit=False)
-                review.author = request.user
-                review.movie_id = movie_id
-                review.save()
-                return redirect("movie_detail", movie_id=movie_id)
+    # 1. Fetch movie details from TMDB API
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    params = {"api_key": settings.TMDB_API_KEY, "language": "en-US"}
+    response = requests.get(url, params=params)
+    
+    if response.status_code != 200:
+        movie = None  # fallback if movie not found
     else:
-        form = ReviewForm()
+        movie = response.json()
+    
+    # check if the user is authenticated and has a review
+    user_review = None
+    if request.user.is_authenticated:
+        user_review = Review.objects.filter(
+            movie_id=movie_id, author=request.user
+        ).first()
 
-    # Fetch movie from TMDB for movie details
-    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={settings.TMDB_API_KEY}&language=en-US"
-    response = requests.get(url)
-    data = response.json()
-    user_review = Review.objects.filter(movie_id=movie_id, author=request.user).first()
-
-
+    # 3. Render template with movie and review info
     context = {
-        "movie": data,
-        "reviews": reviews,
-        "form": form,
-        "user_review": user_review,
+        "movie": movie,
+        "user_review": user_review
     }
     return render(request, "movies/movie_detail.html", context)
 
