c9f0e72a36135ec0eaf31a0a9933617421f0dc14
Rachel Davey <racheld9999@outlook.com>
2025-09-28 16:33:22 +0200

    a

--- movie_tracker/movie_tracker/wsgi.py
+++ movie_tracker/movie_tracker/wsgi.py
@@ -1,4 +1,9 @@
 import os
+import sys
+
+# Add outer movie_tracker folder to sys.path
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(__file__)), 'movie_tracker'))
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movie_tracker.settings')
