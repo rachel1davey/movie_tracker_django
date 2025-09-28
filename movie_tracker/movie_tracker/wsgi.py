ed501c58bafc4576dd81066d59f89dfe51798c13
Rachel Davey <racheld9999@outlook.com>
2025-09-28 16:05:52 +0200

    Fix sys.path configuration in wsgi.py to correctly reference the outer movie_tracker folder

--- movie_tracker/movie_tracker/wsgi.py
+++ movie_tracker/movie_tracker/wsgi.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 # Add outer movie_tracker folder to sys.path
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'movie_tracker'))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(__file__)), 'movie_tracker'))
 
 from django.core.wsgi import get_wsgi_application
 
