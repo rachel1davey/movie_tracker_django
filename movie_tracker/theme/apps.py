fc885b68bb847ecd0600e1ae55274411d4449841
Rachel Davey <racheld9999@outlook.com>
2025-09-28 12:57:34 +0200

    Update ThemeConfig to set default_auto_field and correct app name to 'movie_tracker.theme'

--- movie_tracker/theme/apps.py
+++ movie_tracker/theme/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class ThemeConfig(AppConfig):
-    name = 'theme'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'movie_tracker.theme'   # ✅ Correct
