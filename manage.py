c76131b5a292925150b962c8c0d45f16525d53bd
Rachel Davey <racheld9999@outlook.com>
2025-09-28 16:08:48 +0200

    Remove unnecessary sys.path modifications in manage.py and wsgi.py

--- manage.py
+++ manage.py
@@ -2,9 +2,6 @@
 import os
 import sys
 
-# Add outer movie_tracker folder to sys.path
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'movie_tracker'))
-
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movie_tracker.settings')
     from django.core.management import execute_from_command_line
